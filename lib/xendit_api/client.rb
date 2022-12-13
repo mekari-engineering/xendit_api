ed832347fa0a0719cff64f5a6f9883cb797ac6d1
Vincent Wijaya <vincent.wijaya@mekari.com>
2022-12-13 08:59:23 +0700

    KRED-361 update body logging

--- lib/xendit_api/client.rb
+++ lib/xendit_api/client.rb
@@ -22,7 +22,7 @@ module XenditApi
 
         logger = find_logger(options[:logger])
         if logger
-          connection.response :logger, logger, { headers: false, bodies: true } do |log|
+          connection.response :logger, logger, { headers: false, bodies: { request: true, response: true } } do |log|
             filtered_logs = options[:filtered_logs]
             if filtered_logs.respond_to?(:each)
               filtered_logs.each do |filter|
