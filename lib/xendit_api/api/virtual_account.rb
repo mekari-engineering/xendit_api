f65a9d582278343e62074ff70d3d668a1991f5f5
Lam Doan <ngoclamvt123@gmail.com>
2022-12-22 08:30:08 +0700

    fix failed test

--- lib/xendit_api/api/virtual_account.rb
+++ lib/xendit_api/api/virtual_account.rb
@@ -20,7 +20,7 @@ module XenditApi
         update_path = "#{PATH}/#{id}"
         one_year = 31_556_952
         expired_date = Time.now - one_year
-        params = { expiration_date: expired_date.utc.iso8601 }
+        params = { expiration_date: expired_date.iso8601 }
         response = client.patch(update_path, params, headers)
         virtual_account_params = permitted_virtual_account_params(response)
         XenditApi::Model::VirtualAccount.new(virtual_account_params)
