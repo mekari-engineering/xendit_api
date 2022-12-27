ac4d7190c9249e95f146b7a5860105c25e4a6e97
Lam Doan <ngoclamvt123@gmail.com>
2022-12-27 08:16:57 +0700

    list transaction API

---
+++ lib/xendit_api/api/transaction.rb
@@ -0,0 +1,23 @@
+require 'xendit_api/api/base'
+require 'xendit_api/model/transaction'
+
+module XenditApi
+  module Api
+    class Transaction < XenditApi::Api::Base
+      PATH = '/transactions'.freeze
+
+      def list(query_string = '', headers = {})
+        transactions_path = query_string.to_s.empty? ? PATH : "#{PATH}?#{query_string}"
+        response = @client.get(transactions_path, nil, headers)
+
+        data = response['data'].map do |transaction|
+          XenditApi::Model::Transaction.new(transaction)
+        end
+
+        next_query = response['has_more'] ? URI.parse(response['links'].find{|link| link['rel'] == 'next'}['href']).query : nil
+        
+        OpenStruct.new(data: data, next_query: next_query)
+      end
+    end
+  end
+end
