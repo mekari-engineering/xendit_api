f74774bccd13302e0300d952208977fc7c6d4df3
rikyhidayat21 <rikyhidayat21@gmail.com>
2023-05-22 13:32:55 +0700

    KRED-601 add rspec feature not available

--- spec/xendit_api/api/report_spec.rb
+++ spec/xendit_api/api/report_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe XenditApi::Api::Report do
-  let(:client) { XenditApi::Client.new("xnd_development_AOQryoQ1J5SzSFzMEJ24sYqWUDucDfeKVzjIbtegLYK7t1YsBw4JMm9H9LHvm") }
+  let(:client) { XenditApi::Client.new }
 
   describe '#create' do
     it 'returns expected response' do
       VCR.use_cassette('xendit/report/create/success') do
-        report_api = described_class.new(client)  
-        response  = report_api.create(
-          type: "BALANCE_HISTORY"
+        report_api = described_class.new(client)
+        response = report_api.create(
+          type: 'BALANCE_HISTORY'
         )
         expect(response).to be_instance_of XenditApi::Model::Report
       end
@@ -16,18 +16,31 @@ RSpec.describe XenditApi::Api::Report do
 
     it 'return error invalid date range' do
       VCR.use_cassette('xendit/report/create/invalid_date_range') do
-        report_api = described_class.new(client)  
+        report_api = described_class.new(client)
         params = {
-          type: "BALANCE_HISTORY",
+          type: 'BALANCE_HISTORY',
           filter: {
-            from: "2021-01-23T04:01:55.574Z",
-            to: "2021-06-23T04:01:55.574Z"
+            from: '2021-01-23T04:01:55.574Z',
+            to: '2021-06-23T04:01:55.574Z'
           }
         }
 
         expect do
           report_api.create(params)
         end.to raise_error XenditApi::Errors::Report::InvalidDateRange
+      end
+    end
+
+    it 'return error feature not available' do
+      VCR.use_cassette('xendit/report/create/feature_not_available') do
+        report_api = described_class.new(client)
+        params = {
+          type: 'BALANCE_HISTORY'
+        }
+
+        expect do
+          report_api.create(params)
+        end.to raise_error XenditApi::Errors::Report::FeatureNotAvailable
       end
     end
   end
